2de8bbe9304d23ecef7733b7bd08a5c276b636d3
Samsung <cemaky3>
2023-03-09 15:37:52 +0300

    save multiple notes

--- Secret Diary/task/src/main/java/org/hyperskill/secretdiary/MainActivity.kt
+++ Secret Diary/task/src/main/java/org/hyperskill/secretdiary/MainActivity.kt
@@ -1,11 +1,12 @@
 package org.hyperskill.secretdiary
 
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.datetime.*
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.datetime.TimeZone.Companion.currentSystemDefault
 import org.hyperskill.secretdiary.databinding.ActivityMainBinding
 
 
@@ -13,16 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.btnSave.setOnClickListener {
             if (binding.etNewWriting.text.trim().isNotEmpty()) {
-                binding.tvDiary.text = binding.etNewWriting.editableText
+                var date = Clock.System.now().toLocalDateTime(currentSystemDefault()).toString().replace("T"," ").substringBefore(".")
+                binding.tvDiary.text = "${date}\n${binding.etNewWriting.editableText}${
+                    if (binding.tvDiary.text.isEmpty()) "" else "\n\n${binding.tvDiary.text}"
+                }"
                 binding.etNewWriting.text.clear()
             } else {
                 Toast.makeText(this, "Empty or blank input cannot be saved", Toast.LENGTH_SHORT).show()
